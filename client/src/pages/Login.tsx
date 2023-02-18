eaf87a3118a1ddda6acd1897929b37a26a3ea65d
Pengpa0012 <peng.ilikha@gmail.com>
2023-02-18 19:19:23 +0800

    update form style

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,12 +5,12 @@ import { Link, useNavigate } from 'react-router-dom'
 export const Login = () => {
   return (
     <div className="grid place-items-center min-h-screen">
-      <form className="flex flex-col text-center">
+      <form className="flex flex-col text-center w-full max-w-xl px-2">
         <h1 className="text-3xl mb-6">MERN SOCIAL APP</h1>
-        <input type="text" placeholder="Username" className="p-2 rounded-md mb-2 max-w-xs" />
-        <input type="password" placeholder="Password" className="p-2 rounded-md mb-2 max-w-xs" />
-        <button type="submit" className="my-2 bg-green-500 p-2 rounded-md">Login</button>
-        <p className="text-sm">Create an account? click <Link to="/signup" className="text-blue-200 text-underline">here</Link></p>
+        <input type="text" placeholder="Username" className="p-2 rounded-md mb-2" />
+        <input type="password" placeholder="Password" className="p-2 rounded-md mb-2" />
+        <button type="submit" className="mb-2 bg-green-500 hover:bg-green-600 p-2 rounded-md">Login</button>
+        <p className="text-md">Create an account? click <Link to="/signup" className="text-blue-200 text-underline">here</Link></p>
       </form>
     </div>
   )
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 export const Signup = () => {
   return (
     <div className="grid place-items-center min-h-screen">
-      <form className="flex flex-col text-center">
+      <form className="flex flex-col text-center w-full max-w-xl px-2">
         <h1 className="text-3xl mb-6">MERN SOCIAL APP</h1>
-        <input type="text" placeholder="Username" className="p-2 rounded-md mb-2 max-w-xs" />
-        <input type="password" placeholder="Password" className="p-2 rounded-md mb-2 max-w-xs" />
-        <button type="submit" className="my-2 bg-green-500 p-2 rounded-md">Sign up</button>
-        <p className="text-sm">Already have an account? click <Link to="/login" className="text-blue-200 text-underline">here</Link></p>
+        <input type="text" placeholder="Username" className="p-2 rounded-md mb-2" />
+        <input type="password" placeholder="Password" className="p-2 rounded-md mb-2" />
+        <button type="submit" className="mb-2 bg-green-500 hover:bg-green-600 p-2 rounded-md">Signup</button>
+        <p className="text-md">Already have an account? click <Link to="/login" className="text-blue-200 text-underline">here</Link></p>
       </form>
     </div>
   )
