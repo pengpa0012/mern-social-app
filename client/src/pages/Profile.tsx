e62c90b2c64146f1c496a9cca58a8b6390016802
Pengpa0012 <peng.ilikha@gmail.com>
2023-02-19 14:04:18 +0800

    update profile

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { Post } from '../Components/Post'
 
 export const Profile = () => {
   const {id} = useParams()
   console.log(id)
   return (
     <div className="p-4">
-      <div className="flex">
-        <h1>User IMG</h1>
-        <ul className='flex flex-1 bg-red-500'>
+      <div className="flex py-24">
+        <h1 className="mr-28">User IMG</h1>
+        <ul className='grid grid-cols-2 gap-12'>
           <li>List</li>
           <li>List</li>
           <li>List</li>
           <li>List</li>
         </ul>
       </div>
+      <h2>Your posts</h2>
+      <Post />
     </div>
   )
 }
