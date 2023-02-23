2fb0890b56b581d3387c0060a10801863de44242
Pengpa0012 <peng.ilikha@gmail.com>
2023-02-23 19:27:10 +0800

    update dropdown

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -16,12 +16,12 @@ export const Header = () => {
     <div className="flex justify-between p-4">
       <Link to="/">Home</Link>
       <div className="relative">
-        <h2 onClick={() => setShowDrawer(!showDrawer)}>LOGO</h2>
-        <div className={`absolute ${showDrawer ? "block" : "hidden"}`}>
+        <h2 onClick={() => setShowDrawer(!showDrawer)} className="cursor-pointer">LOGO</h2>
+        <div className={`absolute -left-6 bg-gray-700 rounded-md ${showDrawer ? "block" : "hidden"}`}>
           <Link to={`/profile/${username}`}>
-            <h2 className="mr-4">Profile</h2>
+            <h2 className="mr-4 hover:bg-gray-800 w-full p-2">Profile</h2>
           </Link>
-          <h2 onClick={() => onLogout()}>Logout</h2>
+          <h2 onClick={() => onLogout()} className="p-2 w-full hover:bg-gray-800 cursor-pointer">Logout</h2>
         </div>
       </div>
     </div>
