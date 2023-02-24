1d84451c3d219fcc660aac7f86b18a59d9dd6fa4
Pengpa0012 <peng.ilikha@gmail.com>
2023-02-24 20:03:37 +0800

    update profile, post

--- client/src/Components/Post.tsx
+++ client/src/Components/Post.tsx
@@ -108,19 +108,19 @@ export const Post = ({posts, allPost, setAllPosts, showComment, setShowComment}:
           <div className="mb-4 bg-white/10 p-4 rounded-md relative" key={i}>
             {username == id && <button className="absolute -top-[5px] -right-[5px] w-8 h-8 bg-red-500 hover:bg-red-600 grid place-items-center rounded-full cursor-pointer" onClick={() => onDeletePost(item)}>&#x2715;</button>}
             <div className="flex justify-between items-center mb-4">
-              <h2 className="text-3xl" onClick={() => navigate(`/profile/${item.username}`)}>{item.username}</h2>
-              <p className="text-sm text-white/50">{dayjs(item.date).format("MMM DD, YYYY h:mm a")}</p>
+              <h2 className="text-3xl" onClick={() => navigate(`/profile/${item?.username}`)}>{item?.username}</h2>
+              <p className="text-sm text-white/50">{dayjs(item?.date).format("MMM DD, YYYY h:mm a")}</p>
             </div>  
-            <p className="text-md text-white/70 break-words">{item.description}</p>
+            <p className="text-md text-white/70 break-words">{item?.description}</p>
             <div className={`flex mt-4`}>
               <button className="mr-4 text-white/50 relative" onClick={() => onLikePost(item)}>
-               <span className={item.like.find((user: any) => user.user == userId) ? "text-blue-400 font-bold" : ""}>Like</span>
-               {item.like.length > 0 ? <span className="absolute -top-[3px] -right-[12px] -z-10 bg-white w-4 h-4 rounded-full text-xs grid place-items-center text-blue-500 font-bold">{item.like.length}</span>
+               <span className={item?.like.find((user: any) => user.user == userId) ? "text-blue-400 font-bold" : ""}>Like</span>
+               {item?.like.length > 0 ? <span className="absolute -top-[3px] -right-[12px] -z-10 bg-white w-4 h-4 rounded-full text-xs grid place-items-center text-blue-500 font-bold">{item?.like.length}</span>
                : undefined}
               </button>
               <button className="text-white/50 relative" onClick={() => setShowComment({show: true, index: i})}>
                 Comment
-                {item.comments.length > 0 ? <span className="absolute -top-[3px] -right-[12px] -z-10 bg-red-500 w-4 h-4 rounded-full text-xs grid place-items-center text-white font-bold">{item.comments.length}</span>
+                {item?.comments.length > 0 ? <span className="absolute -top-[3px] -right-[12px] -z-10 bg-red-500 w-4 h-4 rounded-full text-xs grid place-items-center text-white font-bold">{item?.comments.length}</span>
                 : undefined}
                 </button>
             </div>
@@ -129,13 +129,13 @@ export const Post = ({posts, allPost, setAllPosts, showComment, setShowComment}:
               <div className="p-4 bg-black/20 rounded-md mt-4">
                 <div className="max-h-[500px] overflow-auto mb-2">
                   {
-                    item.comments.map((item: any, i: number) => (
+                    item?.comments.map((item: any, i: number) => (
                       <div className="my-2 bg-black/10 p-3 rounded-md" key={i}>
                         <div className="flex items-center justify-between">
-                          <h1 className="mr-2">{item.name}</h1>
-                          <p className="text-xs text-white/50">{dayjs(item.date).format("MMM DD, YYYY h:mm a")}</p>
+                          <h1 className="mr-2">{item?.name}</h1>
+                          <p className="text-xs text-white/50">{dayjs(item?.date).format("MMM DD, YYYY h:mm a")}</p>
                         </div>
-                        <p>{item.text}</p>
+                        <p>{item?.text}</p>
                       </div>
                     ))
                   }
